a4592e60673a9d93d63b8ed7fe3c4e722bcd874e
stevensadh95 <stevensadh95@gmail.com>
2018-04-04 22:17:26 -0400

    Added stuff to start out with

--- main flask.py
+++ main flask.py
@@ -1,13 +1,59 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template,redirect,url_for
+from .forms import SignInForm
+
+from flask_login import LoginManager,UserMixin
+
+
+
+
+import sql_connection as db
+
 
 app = Flask(__name__)
 
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.get(user_id)
+
 @app.route('/')
 def my_form():
-    return render_template('my-form.html')
+    return render_template('index.html')
 
-@app.route('/', methods=['POST'])
-def my_form_post():
-    text = request.form['text']
-    processed_text = text.upper()
-    return processed_text
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+            error = 'Invalid Credentials. Please try again.'
+        else:
+            return redirect(url_for('secret'))
+    return render_template('login.html', error=error)
+
+
+@app.route('/create_account')
+def create_account():
+    return render_template('/create_account.html')
+
+
+# @app.route('/create_account', methods=['POST'])
+# def my_form_post():
+#     text = request.form['text']
+#     processed_text = text.upper()
+#     print (processed_text)
+#     #return processed_text
+#     return render_template('create_account.html')
+
+if __name__ == "__main__":
+    app.run()
+
+
+# gymder user class definition for login
+class User(UserMixin):
+    def __init__(self,email,password):
+        self._password=password
+        self._email=email
+
+
--- sql_connection.py
+++ sql_connection.py
@@ -1,24 +1,63 @@
 import sqlite3
+from flask import request,url_for,redirect,render_template
 
 connection = sqlite3.connect('gymder.db')
 
 cursor = connection.cursor()
 
-# primary key auto increments without putting it there
+
 # cursor.execute("""CREATE TABLE user(
-#             user_id INTEGER PRIMARY KEY, name TEXT, email TEXT, state TEXT, sex TEXT,
+#             user_id INTEGER PRIMARY KEY, password TEXT, name TEXT, email TEXT, state TEXT, sex TEXT,
 #             birthday DATE, picture BLOB, bio BLOB
 #             )""")
 #
 # cursor.execute("""CREATE TABLE match(
 #             user_id TEXT, other_id TEXT, likes text, date_judged DATE, judged TEXT
 #             )""")
+#
+# # for login verification.
+#
+# cursor.execute("""CREATE TABLE login(
+#             email TEXT PRIMARY KEY NOT NULL, password TEXT NOT NULL
+#             )""")
+
+def login():
+    error = None
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        completion = validate_credentials(username, password)
+        if completion ==False:
+            error = 'Invalid Credentials. Please try again.'
+        else:
+            return redirect(url_for('secret'))
+    return render_template('login.html', error=error)
+
+def validate_credentials(username, password):
+    connection = sqlite3.connect('gymder.db')
+    with connection:
+                cur = connection.cursor()
+                cur.execute("SELECT * FROM login")
+                rows = cur.fetchall()
+                for row in rows:
+                    user_db = row[0]
+                    password_db = row[1]
+                    if user_db==username:
+                        return password_db==password
+    return False
 
 
-def add_user(name,email,state,sex,birthday,picture=None,bio=None):
+
+
+def add_user(name,email,password,state,sex,birthday,picture=None,bio=None):
     cursor.execute('''INSERT INTO user(name,email,state,sex,birthday,picture,bio)
      VALUES (?,?,?,?,?,?,?)''', (name, email, state, sex, birthday, picture,bio))
 
+    cursor.execute('''INSERT INTO login(email,password)
+         VALUES (?,?)''', (email, password))
+
+
+# needs some work to determine which attributes to edit
 def edit_user(user_id,name,**attributes):
     cursor.execute('''UPDATE user
     SET name='{}'
@@ -26,10 +65,15 @@ def edit_user(user_id,name,**attributes):
     connection.commit()
 
 
-
 def match(user,other,isLiked):
-    if is_match(user,other):
-        pass # handle match
+    # if is_match(user,other):
+    #     pass # handle match
+    cursor.execute('''INSERT INTO match(user_id,other_id,likes)
+         VALUES (?,?,?)''', (user, other, isLiked))
+
+def like_user(user,other,isLiked):
+    cursor.execute('''INSERT INTO match(user_id,other_id,likes)
+         VALUES (?,?,?)''', (user, other, isLiked))
 
 
 def is_match(user,other):
@@ -40,10 +84,33 @@ def is_match(user,other):
     return user_likes_other and other_likes_user
 
 
+# should work
+def get_users_to_judge(user_id):
+    return cursor.execute('''SELECT user_id FROM user WHERE user_id != {x} AND user_id NOT IN
+    (SELECT m.other_id FROM match m where m.user_id = {x})'''.format(x = user_id))
 
-edit_user(1,"Bilbo")
+def delete_user(user_id):
+    cursor.execute('''DELETE FROM user WHERE user_id = {}'''.format(user_id))
 
- # x = cursor.execute('''SELECT * from user where user_id=1''')
+def delete_matches_by_user_id(user_id):
+    cursor.execute('''DELETE FROM match WHERE user_id = {}'''.format(user_id))
+
+
+x = cursor.execute('''SELECT * from match''')
+for i in cursor:
+    print("\n")
+    for j in i:
+        print(j)
+
+print("Other Users not matched yet")
+y = get_users_to_judge(2)
+
+for i in y:
+    for e in i:
+        print(e)
+
+print("every user in DB")
+
 x = cursor.execute('''SELECT * from user''')
 for i in cursor:
     print("\n")
@@ -51,7 +118,7 @@ for i in cursor:
         print(j)
 
 # example call for add_user
-add_user('Steven Sadhwani', "sadhwani@mail.usf.edu", "NH", "Male", "December 8 1995", "djk")
+# add_user('Steven Sadhwani', "sadhwani@mail.usf.edu", "NH", "Male", "December 8 1995", "djk")
 connection.commit() # need to commit changes.
 
 
