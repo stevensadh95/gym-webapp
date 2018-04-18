2cf7783f8e990efffa27704d7fcc574c6e851d3a
stevensadh95 <stevensadh95@gmail.com>
2018-04-18 16:52:37 -0400

    registration now adds user profile to DB

--- app.py
+++ app.py
@@ -3,7 +3,10 @@ from flask_sqlalchemy import SQLAlchemy
 import tempfile
 import os.path
 from flask_login import LoginManager, login_user, login_required, logout_user, UserMixin,current_user
-from forms import SignupForm
+from forms import SignupForm,ProfileForm
+import sqlite3
+
+
 
 app = Flask(__name__)
 db = SQLAlchemy(app)
@@ -39,6 +42,17 @@ class User(db.Model, UserMixin):
         return str(self.username)
 
 
+class Profile():
+    def __init__(self,username,name,email,state,sex,birthday,picture=None,bio=None):
+        self.username = username
+        self.name=name
+        self.email=email
+        self.state=state
+        self.sex=sex
+        self.birthday=birthday
+        self.picture=picture
+        self.bio=bio
+
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -48,9 +62,30 @@ def index():
 def register():
     form = SignupForm()
 
+    profile = ProfileForm()
+
     if request.method == 'GET':
-        return render_template("sign_up.html", form=form)
+        return render_template("sign_up.html", form=form,profile=profile)
     if request.method == 'POST':
+
+        if profile.validate_on_submit():
+
+
+
+
+            connection = sqlite3.connect('gymder.db')
+            cursor = connection.cursor()
+
+            if cursor.execute("SELECT * FROM user WHERE username='{}'".format(profile.username.data)).fetchall():
+                flash("Username Already Exists", "danger")
+                return redirect(url_for("register"))
+
+            user_profile = Profile(profile.username.data,profile.name.data,profile.email.data,profile.state.data,
+                    profile.sex.data,profile.birthday.data, profile.picture.data,profile.bio.data)
+            add_user(cursor,user_profile)
+            connection.commit()
+
+
         if form.validate_on_submit():
             if User.query.filter_by(username=form.username.data).first():
                 flash("User Already Exists", "danger")
@@ -63,7 +98,7 @@ def register():
 
                 flash("Registered successfully", "info")
                 #return redirect(url_for("index"))
-                return render_template("sign_up.html", form=form)
+                return render_template("sign_up.html", form=form,profile=profile)
 
         else:
             flash("Form didn't validate", "danger")
@@ -78,10 +113,12 @@ def logout():
 
 @app.route('/matches')
 def match_list():
-    id = current_user.username
-
-    # matches = get_matches()
-    return render_template("matches.html",id=id)
+    username = current_user.username
+    connection = sqlite3.connect('gymder.db')
+    cursor = connection.cursor()
+    matches = get_user(cursor,username)
+    connection.commit()
+    return render_template("matches.html",id=id,matches=matches)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -114,6 +151,88 @@ def login():
 #     #user_id = get from session
 #     #my_matches=get_matches(user_id)
 #     return render_template("matches.html",profiles=my_matches)
+
+
+# cursor.execute("""CREATE TABLE user(
+#             username TEXT PRIMARY KEY, name TEXT, email TEXT, state TEXT, sex TEXT,
+#             birthday DATE, picture BLOB, bio BLOB
+#             )""")
+#
+# cursor.execute("""CREATE TABLE match(
+#             username TEXT, other_id TEXT, likes text, date_judged DATE, judged TEXT
+#             )""")
+
+
+def add_user(cursor,p):
+    cursor.execute('''INSERT INTO user(username,name,email,state,sex,birthday,picture,bio)
+     VALUES (?,?,?,?,?,?,?,?)''', (p.username, p.name,p.email, p.state, p.sex, p.birthday, p.picture, p.bio))
+
+# needs some work to determine which attributes to edit
+def edit_user(cursor,user_id,name,**attributes):
+    cursor.execute('''UPDATE user
+    SET name='{}'
+    WHERE user_id = {}'''.format(name, user_id))
+    #connection.commit()
+
+
+def like_user(cursor,user,other,isLiked):
+    cursor.execute('''INSERT INTO match(username,other_id,likes)
+         VALUES (?,?,?)''', (user, other, isLiked))
+    #connection.commit()
+
+'''
+Returns matches that user has as tuples
+'''
+
+def get_matches(cursor,user):
+    tuples = cursor.execute("""
+    SELECT * from user where username=
+    (SELECT other_id FROM match WHERE username='{user}'
+    AND likes='Yes' INTERSECT SELECT username FROM match WHERE other_id='{user}' AND likes='Yes')""".format(user=user)).fetchall()
+    return tuples
+
+
+def get_users_to_judge(cursor,username):
+    return cursor.execute('''SELECT username FROM user WHERE username != {x} AND username NOT IN
+    (SELECT m.other_id FROM match m where m.username = {x})'''.format(x = username))
+
+def delete_user(cursor,username):
+    cursor.execute("DELETE FROM user WHERE username = '{}'".format(username))
+    delete_matches_by_username(username)
+    #connection.commit()
+
+def delete_matches_by_username(cursor,username):
+    cursor.execute("DELETE FROM match WHERE username = '{}'".format(username))
+    cursor.execute("DELETE FROM match WHERE other_id = '{}'".format(username))
+    #connection.commit()
+
+def get_user(cursor,username):
+    return cursor.execute("SELECT * from user where username='{}'".format(username)).fetchall()
+
+
+
+#connection.commit() # need to commit changes.
+
+
+
+# print("every user in DB")
+# tuples = cursor.execute('''SELECT * from user''').fetchall()
+# for tuple in tuples:
+#     print(tuple)
+
+# example call for add_user
+# add_user('sadhwani', "steven","sadhwani@mail.usf.edu", "NH", "Male", "December 8 1995", "djk", " ffs)
+
+
+
+# For debugging match table
+# print(cursor.execute('''SELECT * from match''').fetchall())
+
+# To test get_matches function against first DB element
+# print(get_matches("<username>"))
+
+#
+# connection.close()
 
 
 if __name__ == '__main__':
--- forms.py
+++ forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms import StringField, PasswordField, RadioField, SubmitField,SelectField
 from wtforms.validators import DataRequired,ValidationError
+from wtforms.fields.html5 import DateField
 
 class LoginForm(FlaskForm):
     username= StringField("Username", validators=[DataRequired()])
@@ -11,6 +12,16 @@ class SignupForm(FlaskForm):
     username = StringField("Username", validators=[DataRequired()])
     password = PasswordField("Password", validators=[DataRequired()])
     submit = SubmitField('Sign up')
+
+class ProfileForm(FlaskForm):
+    username = StringField("Username", validators=[DataRequired()])
+    name = StringField("Name", validators=[DataRequired()])
+    email = StringField("Email")
+    state = SelectField("State",choices=[('NH','Fl')])
+    sex = RadioField("Sex",choices=[('Male','Male'),('Female','Female')])
+    birthday = DateField("DatePicker")
+    picture = StringField("Image")
+    bio = StringField("Bio")
 
     # def validate_username(self,username):
     #     user = User.query.filter_by(username=username.data).first()
